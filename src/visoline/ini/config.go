84401b67804665e19304c7d6b78d93ce29a97a1a
chengziqing <feixings@gmail.com>
2013-05-03 10:37:40 +0800

    提交源码

---
+++ src/visoline/ini/config.go
@@ -0,0 +1,211 @@
+// Copyright (c) 2009-2010 Jim Teeuwen.
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+//     1. The origin of this software must not be misrepresented; you must not
+//     claim that you wrote the original software. If you use this software
+//     in a product, an acknowledgment in the product documentation would be
+//     appreciated but is not required.
+//
+//     2. Altered source versions must be plainly marked as such, and must not be
+//     misrepresented as being the original software.
+//
+//     3. This notice may not be removed or altered from any source distribution.
+package ini
+
+import "strings"
+
+type Config struct {
+	Sections map[string]*Section
+}
+
+func NewConfig() *Config {
+	c := new(Config)
+	c.Sections = make(map[string]*Section)
+	return c
+}
+
+func (this *Config) Clear() {
+	this.Sections = make(map[string]*Section)
+}
+
+func (this *Config) AddComment(section, val string) {
+	section = strings.ToLower(section)
+
+	if _, ok := this.Sections[section]; !ok {
+		this.Sections[section] = NewSection(section)
+	}
+
+	this.Sections[section].AddComment(val)
+}
+
+func (this *Config) Set(section, key string, val interface{}) {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if _, ok := this.Sections[section]; !ok {
+		this.Sections[section] = NewSection(section)
+	}
+
+	this.Sections[section].Set(key, val)
+}
+
+func (this *Config) S(section, key, defval string) string {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.S(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) I(section, key string, defval int) int {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.I(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) I8(section, key string, defval int8) int8 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.I8(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) I16(section, key string, defval int16) int16 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.I16(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) I32(section, key string, defval int32) int32 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.I32(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) I64(section, key string, defval int64) int64 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.I64(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) U(section, key string, defval uint) uint {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.U(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) U8(section, key string, defval uint8) uint8 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.U8(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) U16(section, key string, defval uint16) uint16 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.U16(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) U32(section, key string, defval uint32) uint32 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.U32(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) U64(section, key string, defval uint64) uint64 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.U64(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) F32(section, key string, defval float32) float32 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.F32(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) F64(section, key string, defval float64) float64 {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.F64(key, defval)
+	}
+
+	return defval
+}
+
+func (this *Config) B(section, key string, defval bool) bool {
+	section = strings.ToLower(section)
+	key = strings.ToLower(key)
+
+	if s, ok := this.Sections[section]; ok {
+		return s.B(key, defval)
+	}
+
+	return defval
+}
---
+++ src/visoline/ini/ini.go
@@ -0,0 +1,150 @@
+// Copyright (c) 2009-2010 Jim Teeuwen.
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+//     1. The origin of this software must not be misrepresented; you must not
+//     claim that you wrote the original software. If you use this software
+//     in a product, an acknowledgment in the product documentation would be
+//     appreciated but is not required.
+//
+//     2. Altered source versions must be plainly marked as such, and must not be
+//     misrepresented as being the original software.
+//
+//     3. This notice may not be removed or altered from any source distribution.
+
+/*
+This package parses run of the mill ini configutation files. It allows loading
+and saving. It supports sections and preservation of comments.
+*/
+package ini
+
+import "io/ioutil"
+import "bytes"
+import "strings"
+import "visoline/mahonia"
+import "io"
+
+var strPairSeparator []byte = []byte{'='}
+var strNewline []byte = []byte{'\n'}
+var strComment []byte = []byte{';'}
+
+func Load(file string, isGBK bool) (cfg *Config, err error) {
+	var data []byte
+	var pair [][]byte
+	var n int
+
+	if data, err = ioutil.ReadFile(file); err != nil {
+		return
+	}
+	//读取GBK类型文件
+	if isGBK {
+		decode := mahonia.NewDecoder("GBK")
+		encode := mahonia.NewEncoder("UTF-8")
+		r := decode.NewReader(bytes.NewReader(data))
+		buf := bytes.NewBuffer([]byte(""))
+		w := encode.NewWriter(buf)
+		io.Copy(w, r)
+		data = buf.Bytes()
+	}
+	cfg = &Config{}
+	cfg.Sections = make(map[string]*Section)
+
+	section := "_"
+	addSection(cfg, section)
+
+	for _, line := range bytes.Split(data, strNewline) {
+		if line = bytes.TrimSpace(line); len(line) == 0 {
+			continue
+		}
+
+		pair = bytes.Split(line, strPairSeparator)
+
+		for i, _ := range pair {
+			pair[i] = bytes.TrimSpace(pair[i])
+		}
+
+		switch {
+		case len(pair) == 1 && len(pair[0]) > 1:
+			switch {
+			case pair[0][0] == ';' || pair[0][0] == '#':
+				cfg.Sections[section].AddComment(string(pair[0][1:]))
+			case pair[0][0] == '[' && pair[0][len(pair[0])-1] == ']':
+				section = string(pair[0][1 : len(pair[0])-1])
+				addSection(cfg, section)
+			}
+		case len(pair) == 2:
+			if n = bytes.Index(pair[1], strComment); n != -1 {
+				cfg.Sections[section].AddComment(string(pair[1][n+1:]))
+				pair[1] = pair[1][0:n]
+			}
+			addPair(cfg, section, pair)
+		}
+	}
+
+	return
+}
+
+func Save(file string, isGBK bool, cfg *Config) (err error) {
+	var data []byte
+	buf := bytes.NewBuffer(data)
+
+	// global section _ goes first
+	if s, ok := cfg.Sections["_"]; ok {
+		writeSection(buf, s)
+	}
+
+	for k, v := range cfg.Sections {
+		if k == "_" {
+			continue
+		}
+		writeSection(buf, v)
+	}
+	result := buf.Bytes()
+	if isGBK {
+		encode := mahonia.NewEncoder("GBK")
+		encodeData := encode.ConvertString(string(result))
+		result = []byte(encodeData)
+	}
+	return ioutil.WriteFile(file, result, 0600)
+}
+
+func writeSection(buf *bytes.Buffer, s *Section) {
+	if s.Name != "_" {
+		buf.WriteString(s.String())
+		buf.WriteByte('\n')
+	}
+
+	for _, c := range s.Comments {
+		buf.WriteString("; ")
+		buf.WriteString(c)
+		buf.WriteByte('\n')
+	}
+
+	for sk, sv := range s.Pairs {
+		buf.WriteString(sk)
+		buf.WriteString(" = ")
+		buf.WriteString(sv)
+		buf.WriteByte('\n')
+	}
+	buf.WriteByte('\n')
+}
+
+func addSection(cfg *Config, name string) {
+	name = strings.ToLower(name)
+	if _, ok := cfg.Sections[name]; !ok {
+		cfg.Sections[name] = NewSection(name)
+	}
+}
+
+func addPair(cfg *Config, name string, pair [][]byte) {
+	if pair[0] = bytes.TrimSpace(pair[0]); len(pair[0]) == 0 {
+		return
+	}
+	cfg.Sections[name].Pairs[string(pair[0])] = string(bytes.TrimSpace(pair[1]))
+}
